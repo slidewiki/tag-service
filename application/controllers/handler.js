dda2c1691f5fc913905e9bfb8e116b54500d60f1
Serafeim Chatzopoulos <s.chatzopoulos@gmail.com>
2017-03-21 16:42:36 +0200

    fix lint errors

--- application/controllers/handler.js
+++ application/controllers/handler.js
@@ -5,46 +5,46 @@ Handles the requests by executing stuff and replying to the client. Uses promise
 'use strict';
 
 const boom = require('boom'), //Boom gives us some predefined http codes and proper responses
-  slideDB = require('../database/slideDatabase'), //Database functions specific for slides
-  co = require('../common');
+    tagDB = require('../database/tagDatabase'), //Database functions specific for tags
+    co = require('../common');
 
 module.exports = {
-  //Get slide from database or return NOT FOUND
-  getSlide: function(request, reply) {
-    slideDB.get(encodeURIComponent(request.params.id)).then((slide) => {
-      if (co.isEmpty(slide))
-        reply(boom.notFound());
-      else
-        reply(co.rewriteID(slide));
-    }).catch((error) => {
-      request.log('error', error);
-      reply(boom.badImplementation());
-    });
-  },
+    //Get slide from database or return NOT FOUND
+    getTag: function(request, reply) {
+        tagDB.get(encodeURIComponent(request.params.id)).then((slide) => {
+            if (co.isEmpty(slide))
+                reply(boom.notFound());
+            else
+                reply(co.rewriteID(slide));
+        }).catch((error) => {
+            request.log('error', error);
+            reply(boom.badImplementation());
+        });
+    },
 
-  //Create Slide with new id and payload or return INTERNAL_SERVER_ERROR
-  newSlide: function(request, reply) {
-    slideDB.insert(request.payload).then((inserted) => {
-      if (co.isEmpty(inserted.ops[0]))
-        throw inserted;
-      else
-        reply(co.rewriteID(inserted.ops[0]));
-    }).catch((error) => {
-      request.log('error', error);
-      reply(boom.badImplementation());
-    });
-  },
+    //Create Slide with new id and payload or return INTERNAL_SERVER_ERROR
+    newSlide: function(request, reply) {
+        tagDB.insert(request.payload).then((inserted) => {
+            if (co.isEmpty(inserted.ops[0]))
+                throw inserted;
+            else
+                reply(co.rewriteID(inserted.ops[0]));
+        }).catch((error) => {
+            request.log('error', error);
+            reply(boom.badImplementation());
+        });
+    },
 
-  //Update Slide with id id and payload or return INTERNAL_SERVER_ERROR
-  replaceSlide: function(request, reply) {
-    slideDB.replace(encodeURIComponent(request.params.id), request.payload).then((replaced) => {
-      if (co.isEmpty(replaced.value))
-        throw replaced;
-      else
-        reply(replaced.value);
-    }).catch((error) => {
-      request.log('error', error);
-      reply(boom.badImplementation());
-    });
-  },
+    //Update Slide with id id and payload or return INTERNAL_SERVER_ERROR
+    replaceSlide: function(request, reply) {
+        tagDB.replace(encodeURIComponent(request.params.id), request.payload).then((replaced) => {
+            if (co.isEmpty(replaced.value))
+                throw replaced;
+            else
+                reply(replaced.value);
+        }).catch((error) => {
+            request.log('error', error);
+            reply(boom.badImplementation());
+        });
+    },
 };
--- application/routes.js
+++ application/routes.js
@@ -5,71 +5,64 @@ Each route implementes a basic parameter/payload validation and a swagger API do
 'use strict';
 
 const Joi = require('joi'),
-  handlers = require('./controllers/handler');
+    handlers = require('./controllers/handler');
 
 module.exports = function(server) {
-  //Get slide with id id from database and return it (when not available, return NOT FOUND). Validate id
-  server.route({
-    method: 'GET',
-    path: '/slide/{id}',
-    handler: handlers.getSlide,
-    config: {
-      validate: {
-        params: {
-          id: Joi.string().alphanum().lowercase()
-        },
-      },
-      tags: ['api'],
-      description: 'Get a slide'
-    }
-  });
+    
+    server.route({
+        method: 'GET',
+        path: '/tag/{tagName}',
+        handler: handlers.getTag,
+        config: {
+            validate: {
+                params: {
+                    tagName: Joi.string().alphanum().lowercase()
+                },
+            },
+            tags: ['api'],
+            description: 'Get a tag'
+        }
+    });
 
-  //Create new slide (by payload) and return it (...). Validate payload
-  server.route({
-    method: 'POST',
-    path: '/slide/new',
-    handler: handlers.newSlide,
-    config: {
-      validate: {
-        payload: Joi.object().keys({
-          title: Joi.string(),
-          body: Joi.string(),
-          user_id: Joi.string().alphanum().lowercase(),
-          root_deck_id: Joi.string().alphanum().lowercase(),
-          parent_deck_id: Joi.string().alphanum().lowercase(),
-          no_new_revision: Joi.boolean(),
-          position: Joi.number().integer().min(0),
-          language: Joi.string()
-        }).requiredKeys('title', 'body'),
-      },
-      tags: ['api'],
-      description: 'Create a new slide'
-    }
-  });
+    //Create new slide (by payload) and return it (...). Validate payload
+    server.route({
+        method: 'POST',
+        path: '/tag/new',
+        handler: handlers.newTag,
+        config: {
+            validate: {
+                payload: Joi.object().keys({
+                    tagName: Joi.string().alphanum().lowercase(),
+                    name: Joi.string().alphanum().lowercase(),
+                    kind: Joi.string(),
+                    uri: Joi.string(),
+                    userId: Joi.string(),
+                }).requiredKeys('tagName', 'kind'),
+            },
+            tags: ['api'],
+            description: 'Create a new tag'
+        }
+    });
 
-  //Update slide with id id (by payload) and return it (...). Validate payload
-  server.route({
-    method: 'PUT',
-    path: '/slide/{id}',
-    handler: handlers.replaceSlide,
-    config: {
-      validate: {
-        params: {
-          id: Joi.string().alphanum().lowercase()
-        },
-        payload: Joi.object().keys({
-          title: Joi.string(),
-          body: Joi.string(),
-          user_id: Joi.string().alphanum().lowercase(),
-          root_deck_id: Joi.string().alphanum().lowercase(),
-          parent_deck_id: Joi.string().alphanum().lowercase(),
-          no_new_revision: Joi.boolean(),
-          position: Joi.number().integer().min(0),
-          language: Joi.string()
-        }).requiredKeys('title', 'body'),
-      },
-      tags: ['api'],
-      description: 'Replace a slide'
-    }
-  });
+    //Update slide with id id (by payload) and return it (...). Validate payload
+    server.route({
+        method: 'PUT',
+        path: '/tag/{tagName}',
+        handler: handlers.replaceTag,
+        config: {
+            validate: {
+                params: {
+                    tagName: Joi.string().alphanum().lowercase()
+                },
+                payload: Joi.object().keys({
+                    name: Joi.string().alphanum().lowercase(),
+                    kind: Joi.string(),
+                    uri: Joi.string(),
+                    userId: Joi.string(),
+                }),
+            },
+            tags: ['api'],
+            description: 'Replace a tag'
+        }
+    });
 };
